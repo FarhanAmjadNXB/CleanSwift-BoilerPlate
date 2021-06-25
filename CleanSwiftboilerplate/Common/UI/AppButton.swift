0a003841d5d6c6a91523450899511d0a7645894d
Farhan Amjad <>
2021-06-25 18:37:57 +0500

    restructure common

---
+++ CleanSwiftboilerplate/Common/UI/AppButton.swift
@@ -0,0 +1,8 @@
+//
+//  AppButton.swift
+//  CleanSwiftboilerplate
+//
+//  Created by Farhan Amjad on 25.06.21.
+//
+
+import Foundation
---
+++ CleanSwiftboilerplate/Common/UI/Base classes/AppTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AppTableViewController.swift
+//  CleanSwiftboilerplate
+//
+//  Created by Farhan Amjad on 25.06.21.
+//
+
+import UIKit
+
+class AppTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
